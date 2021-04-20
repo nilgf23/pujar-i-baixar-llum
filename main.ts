f7c8d6af1b2a4a86799f3415ef7a793fb050e585
nilgf23 <82875774+nilgf23@users.noreply.github.com>
2021-04-20 12:16:34 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,14 +10,26 @@ led.setBrightness(llum)
 serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
+        if (llum < 255) {
+            llum += 1
+            led.setBrightness(llum)
+            serial.writeLine("" + (llum))
+        } else {
+            soundExpression.giggle.play()
+        }
         llum += 1
         led.setBrightness(llum)
         basic.pause(25)
         serial.writeLine("" + (llum))
     }
-})
-basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
+        if (llum >= 0) {
+            llum += -1
+            led.setBrightness(llum)
+            serial.writeLine("" + (llum))
+        } else {
+            soundExpression.giggle.play()
+        }
         llum += -1
         led.setBrightness(llum)
         basic.pause(25)
